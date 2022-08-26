7cf22908d31723d0059460d49726a493a42738b8
Patrick <patmccaw@unc.edu>
2022-08-25 23:02:16 -0400

    Exercise 1: chardle

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,41 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730559806"
+
+guess: str = input("Enter a 5-character word: ")
+instance: int = 0
+if len(guess) == 5:
+  char: str = input("Enter a single character: ")
+  if len(char) == 1:
+    print("Searching for " + char + " in " + guess)
+    if guess[0] == char:
+     print(char + " found at index 0")
+     if guess[0] == char:
+         instance += 1
+    if guess[1] == char:
+     print(char + " found at index 1")
+     if guess[1] == char:
+         instance += 1
+    if guess[2] == char:
+     print(char + " found at index 2")
+     if guess[2] == char:
+       instance += 1
+    if guess[3] == char:
+     print(char + " found at index 3")
+     if guess[3] == char:
+       instance += 1
+    if guess[4] == char:
+      print(char + " found at index 4")
+      if guess[4] == char:
+       instance += 1
+
+    if instance == 0:
+     print("No instance of " + char + " found in " + guess)
+    if instance == 1:
+      print(str(instance) + " instance of " + char + " found in " + guess)
+    if instance > 1:
+     print(str(instance) + " instances of " + char + " found in " + guess)
+  else:
+    print("Error: Character must be a single character")
+else:
+  print("Error: word must containt 5 letters")
---
+++ lessons/00_stored_program.py
@@ -0,0 +1,6 @@
+"""An example, simple program"""
+
+print("hello world!")
+quiz = 100
+print(quiz)
+print("Goodbye!")
